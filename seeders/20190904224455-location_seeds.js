3c2eb9b8c558d4c62cf94d80497d99e8785ef153
Martin Mercer <28820023+m-mrcr@users.noreply.github.com>
2019-09-06 16:35:45 -0600

    changes seeds

--- seeders/20190904224455-location_seeds.js
+++ seeders/20190904224455-location_seeds.js
@@ -10,7 +10,7 @@ module.exports = {
         url: "https://www.yelp.com/biz/little-owl-coffee-denver?adjust_creative=SFrK1QyyPZbVwjh2vOWSjw&utm_campaign=yelp_api_v3&utm_medium=api_v3_business_lookup&utm_source=SFrK1QyyPZbVwjh2vOWSjw",
         phone: "(720) 328-5574",
         rating: 4.5,
-        review_count: 336,
+        reviewCount: 336,
         categories: [
           "Coffee & Tea",
           "Bakeries"
@@ -30,6 +30,7 @@ module.exports = {
           "0800-1500",
           "0800-1500"
         ],
+        yelpId: 'heaStxibj5y6IIuwuXeNSA',
         createdAt: new Date(),
         updatedAt: new Date()
       },
@@ -40,7 +41,7 @@ module.exports = {
         url: "https://www.yelp.com/biz/tacos-tequila-whiskey-denver-2?adjust_creative=SFrK1QyyPZbVwjh2vOWSjw&utm_campaign=yelp_api_v3&utm_medium=api_v3_business_lookup&utm_source=SFrK1QyyPZbVwjh2vOWSjw",
         phone: "(720) 475-1337",
         rating: 4.0,
-        review_count: 1000,
+        reviewCount: 1000,
         categories: [
           "Mexican"
         ],
@@ -59,6 +60,7 @@ module.exports = {
           "1100-2200",
           "1100-2100"
         ],
+        yelpId: 'EwiMXeyrGX5IPGJZAXuQdg',
         createdAt: new Date(),
         updatedAt: new Date()
       },
@@ -69,16 +71,15 @@ module.exports = {
         url: "https://www.yelp.com/biz/ihop-denver?adjust_creative=SFrK1QyyPZbVwjh2vOWSjw&utm_campaign=yelp_api_v3&utm_medium=api_v3_business_lookup&utm_source=SFrK1QyyPZbVwjh2vOWSjw",
         phone: "(303) 758-1492",
         rating: 3.0,
-        review_count: 68,
+        reviewCount: 68,
         categories: [
           "Breakfast & Brunch",
           "American (Traditional)",
           "Burgers"
         ],
         location: [
-          "1514 York St",
-          "Ste 150",
-          "Denver, CO 80206"
+          "1595 S Colorado Blvd",
+          "Denver, CO 80222"
         ],
         price: "$$",
         hours: [
@@ -90,6 +91,7 @@ module.exports = {
           "0000-0000",
           "0000-0000"
         ],
+        yelpId: 'rNiup7lpPQCqHK71VI',
         createdAt: new Date(),
         updatedAt: new Date()
       },
@@ -100,7 +102,7 @@ module.exports = {
         url: "https://www.yelp.com/biz/highland-tavern-denver?adjust_creative=SFrK1QyyPZbVwjh2vOWSjw&utm_campaign=yelp_api_v3&utm_medium=api_v3_business_lookup&utm_source=SFrK1QyyPZbVwjh2vOWSjw",
         phone: "(303) 433-1990",
         rating: 4.0,
-        review_count: 524,
+        reviewCount: 524,
         categories: [
           "Bars",
           "American (Traditional)"
@@ -119,6 +121,7 @@ module.exports = {
           "1000-0200",
           "1000-0200"
         ],
+        yelpId: 'wmGPi9TKAkWbiWzIhE2U9g',
         createdAt: new Date(),
         updatedAt: new Date()
       },
@@ -129,7 +132,7 @@ module.exports = {
         url: "https://www.yelp.com/biz/crema-coffee-house-denver-2?adjust_creative=6IQ69FnHUmPOvoU64q9Xgw&utm_campaign=yelp_api_v3&utm_medium=api_v3_business_lookup&utm_source=6IQ69FnHUmPOvoU64q9Xgw",
         phone: "",
         rating: 4.5,
-        review_count: 551,
+        reviewCount: 551,
         categories: [
           "Coffee & Tea",
           "Breakfast & Brunch",
@@ -150,6 +153,7 @@ module.exports = {
           "0700-1700",
           "0700-1700"
         ],
+        yelpId: 'kZGotdsAt2Bh9QGmHgrEIg',
         createdAt: new Date(),
         updatedAt: new Date()
       }

60a5398aad18c3132b59711d0ecc14f628e2fce7
Martin Mercer <28820023+m-mrcr@users.noreply.github.com>
2019-09-06 15:13:07 -0600

    adds to tests for yelp search

--- test/search.test.js
+++ test/search.test.js
@@ -11,32 +11,37 @@ describe('Search', () => {
     cleanup()
   });
 
-  // it('GET request for all locations formatted for search page', () => {
-  //   return request(app)
-  //   .get('/api/v1/search/recommendations_search?q=coffee')
-  //   .then(response => {
-  //     expect(response.statusCode).toBe(200)
-  //     expect(response.body[0].id).toBe(1)
-  //     expect(response.body[0].name).toBe('Little Owl Coffee')
-  //     expect(response.body[1].id).toBe(5)
-  //     expect(response.body[1].name).toBe('Crema Coffee House')
-  //   })
-  // })
+  it('GET request for all locations formatted for search page', () => {
+    return request(app)
+    .get('/api/v1/search/recommendations_search?q=coffee')
+    .then(response => {
+      expect(response.statusCode).toBe(200)
+      expect(response.body[0].id).toBe(1)
+      expect(response.body[0].name).toBe('Little Owl Coffee')
+      expect(response.body[1].id).toBe(5)
+      expect(response.body[1].name).toBe('Crema Coffee House')
+    })
+  })
 
-  // it('GET request for all yelp search results formatted for search page - LOCATION', () => {
-  //   return request(app)
-  //   .get('/api/v1/search/yelp_search?term=coffee&location=80202')
-  //   .then(response => {
-  //     expect(response.statusCode).toBe(200)
-  //   })
-  // })
+  it('GET request for all yelp search results formatted for search page - LOCATION', () => {
+    return request(app)
+    .get('/api/v1/search/yelp_search?term=coffee&location=80202')
+    .then(response => {
+      expect(response.statusCode).toBe(200)
+      expect(response.body.businesses.length).toBe(20)
+      expect(Object.keys(response.body.businesses[0])).toContain('id')
+      expect(Object.keys(response.body.businesses[0])).toContain('name')
+    })
+  })
 
   it('GET request for all yelp search results formatted for search page - LAT/LONG', () => {
     return request(app)
     .get('/api/v1/search/yelp_search?term=coffee&latitude=39.7508006&longitude=-104.9965947')
     .then(response => {
       expect(response.statusCode).toBe(200)
-
+      expect(response.body.businesses.length).toBe(20)
+      expect(Object.keys(response.body.businesses[0])).toContain('id')
+      expect(Object.keys(response.body.businesses[0])).toContain('name')
     })
   })
 });

5df1d54cc664e0cb9bbbc2b40250e97f289ea494
Martin Mercer <28820023+m-mrcr@users.noreply.github.com>
2019-09-05 18:06:31 -0600

    adds users/recommendations routes file
    Co-Authored-By: Kyle Cornelissen <kylecornelissen@users.noreply.github.com>

---
+++ routes/api/v1/users/recommendations.js
@@ -0,0 +1,19 @@
+var express = require('express');
+var router = express.Router();
+var Location = require('../../../models').Location;
+var Sequelize = require('sequelize')
+var Op = Sequelize.Op
+var defaultHeader = ["Content-Type", "application/json"]
+
+router.get('/', async function(req, res) {
+  try {
+    let locations = await Location.findAll();
+    res.setHeader(...defaultHeader);
+    res.status(200).send(JSON.stringify(locations));
+  } catch (error) {
+    res.setHeader(...defaultHeader);
+    res.status(500).send({ error })
+  }
+});
+
+module.exports = router;

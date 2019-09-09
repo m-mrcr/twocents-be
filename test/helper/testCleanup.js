c8bc1c30c6424dc1434106c85686079ba7973206
Martin Mercer <28820023+m-mrcr@users.noreply.github.com>
2019-09-09 17:50:06 -0600

    modifies how test cleanup is written
    Co-Authored-By: Kyle Cornelissen <kylecornelissen@users.noreply.github.com>

--- test/helper/testCleanup.js
+++ test/helper/testCleanup.js
@@ -1,5 +1,9 @@
 var Location = require('../../models').Location;
+var User = require('../../models').User;
+var UserLocation = require('../../models').UserLocation;
 
 module.exports = async function cleanup() {
   await Location.destroy({ where: {} })
+  await User.destroy({ where: {} })
+  await UserLocation.destroy({ where: {} })
 }

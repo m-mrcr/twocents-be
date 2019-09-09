31085cd0a9f8f959d60e449786210b73b782cb6c
Martin Mercer <28820023+m-mrcr@users.noreply.github.com>
2019-09-09 16:30:32 -0600

    adds user routes for login and sign up
    Co-Authored-By: Kyle Cornelissen <kylecornelissen@users.noreply.github.com>

--- routes/api/v1/users.js
+++ routes/api/v1/users.js
@@ -1,9 +1,45 @@
 var express = require('express');
 var router = express.Router();
+const fetch = require('node-fetch')
+var crypto = require('crypto')
+var User = require('../../../models').User;
+var Location = require('../../../models').Location;
+var UserLocation = require('../../../models').UserLocation;
+var Sequelize = require('sequelize')
+var Op = Sequelize.Op
+var defaultHeader = ["Content-Type", "application/json"]
+require('dotenv').config();
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get('/login', async function(req, res) {
+  var encrypted = encrypt(req.query.p)
+  try {
+    let user = await User.findOne({ where: { key: encrypted }});
+    res.status(200).send(JSON.stringify(user));
+  } catch (error) {
+    console.log(error)
+    res.status(500).send({ error })
+  }
 });
+
+// router.get('/signup', async function(req, res) {
+//   var encrypted = encrypt(req.query.p)
+//   try {
+//     let user = await User.findOrCreate({
+//       where: {
+//         key: encrypted
+//       }
+//     });
+//     res.status(200).send(JSON.stringify(user));
+//   } catch (error) {
+//     res.status(500).send({ error })
+//   }
+// });
+
+function encrypt(input) {
+  return crypto.createHmac('sha256', process.env.APP_SECRET)
+                   .update(input)
+                   .digest('hex')
+};
+
 
 module.exports = router;

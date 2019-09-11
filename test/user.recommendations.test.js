f8eb5a5cefa088f55d52745b43b28f01c5e528c3
Martin Mercer <28820023+m-mrcr@users.noreply.github.com>
2019-09-11 15:49:17 -0600

    uncomments and comments various things in progress

--- test/user.recommendations.test.js
+++ test/user.recommendations.test.js
@@ -22,16 +22,16 @@ describe('Recommendations', () => {
     })
   });
 
-  it("POST request for specific recommendation", async () => {
-    return await request(app)
-    .post('/api/v1/users/recommendations/44a4e0cd5616b584e6727171016f37bc943d456cb4d4f4f94ca3ff8def77da64?yelpId=UeIXzDWs8YLusMDYElJuSQ')
-    .then(response => {
-      expect(response.statusCode).toBe(201)
-      // expect(Object.keys(response.body[0])).toContain('name')
-      // expect(Object.keys(response.body[0])).toContain('url')
-      // expect(Object.keys(response.body[0])).toContain('hours')
-    })
-  });
+  // it("POST request for specific recommendation", async () => {
+  //   return await request(app)
+  //   .post('/api/v1/users/recommendations/44a4e0cd5616b584e6727171016f37bc943d456cb4d4f4f94ca3ff8def77da64?yelpId=UeIXzDWs8YLusMDYElJuSQ')
+  //   .then(response => {
+  //     expect(response.statusCode).toBe(201)
+  //     // expect(Object.keys(response.body[0])).toContain('name')
+  //     // expect(Object.keys(response.body[0])).toContain('url')
+  //     // expect(Object.keys(response.body[0])).toContain('hours')
+  //   })
+  // });
 
   // it("GET request for all specific user's recommendations", async () => {
   //   return await request(app)

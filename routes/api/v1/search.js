58992612ffe3696eb669f40b23bc76ce382eacd1
Martin Mercer <28820023+m-mrcr@users.noreply.github.com>
2019-09-10 19:35:42 -0600

    adds serializing functionality to search results
    Co-Authored-By: Kyle Cornelissen <kylecornelissen@users.noreply.github.com>

--- routes/api/v1/search.js
+++ routes/api/v1/search.js
@@ -1,15 +1,17 @@
 var Sequelize = require('sequelize')
 var Op = Sequelize.Op
 var Location = require('../../../models').Location;
-var Serializer = require('../serializers/yelpToLocationSerializer.js');
+var Serializer = require('../../../serializers/yelpToLocationSerializer.js');
+var serializer = new Serializer();
 var express = require('express');
-const fetch = require('node-fetch')
+var fetch = require('node-fetch')
 var defaultHeader = ["Content-Type", "application/json"]
 var router = express.Router();
 require('dotenv').config();
 
 router.get('/recommendations_search', async function(req, res) {
   var query = req.query.q
+  res.setHeader(...defaultHeader);
   try {
     let locations = await Location.findAll({
       where: {
@@ -18,10 +20,8 @@ router.get('/recommendations_search', async function(req, res) {
         }
       }
     });
-    res.setHeader(...defaultHeader);
     res.status(200).send(JSON.stringify(locations, ['id', 'name']));
   } catch (error) {
-    res.setHeader(...defaultHeader);
     res.status(500).send({ error })
   }
 });
@@ -38,22 +38,14 @@ router.get('/yelp_search', async function(req, res) {
     if(term) {
       if(latitude && longitude) {
         let results = await fetch(`https://api.yelp.com/v3/businesses/search?term=${term}&latitude=${latitude}&longitude=${longitude}&limit=10`, { method: 'GET', headers: headers})
-          .then(function(response) {
-            return response.json();
-          })
-          .catch(function(error){
-            return error
-          })
-        res.status(200).send(JSON.stringify(results));
+          .then(response => response.json())
+          .catch(error => error)
+        res.status(200).send(JSON.stringify(serializer.truncatedLocationSerializer(results)));
       } else if(location) {
           let results = await fetch(`https://api.yelp.com/v3/businesses/search?term=${term}&location=${location}&limit=10`, {method: 'GET', headers: headers})
-            .then(function(response) {
-              return response.json();
-            })
-            .catch(function(error){
-              return error
-            })
-          res.status(200).send(JSON.stringify(results));
+            .then(response => response.json())
+            .catch(error => error)
+          res.status(200).send(JSON.stringify(serializer.truncatedLocationSerializer(results)));
         } else {
           HandleError();
         }
@@ -65,6 +57,22 @@ router.get('/yelp_search', async function(req, res) {
     }
   }
 );
+
+router.get('/yelp_business', async function(req, res) {
+  try {
+    let yelpId = req.query.yelpId;
+    let headers = {"Authorization": `Bearer ${process.env.YELP_API_KEY}`}
+    if(yelpId) {
+      let results = await fetch(`https://api.yelp.com/v3/businesses/${yelpId}`, { method: 'GET', headers: headers })
+        .then(response => response.json())
+        .catch(error => error)
+      res.setHeader(...defaultHeader);
+      res.status(200).send(JSON.stringify(serializer.locationSerializer(results)));
+    }
+  } catch (error) {
+      res.status(500).send({ error })
+  }
+});
 
 
 

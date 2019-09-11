0501946eb09b99d4751cb6c29fc4c6cfc96b4992
Martin Mercer <28820023+m-mrcr@users.noreply.github.com>
2019-09-10 20:02:22 -0600

    refines tests for login and sign up
    Co-Authored-By: Kyle Cornelissen <kylecornelissen@users.noreply.github.com>

--- test/user.test.js
+++ test/user.test.js
@@ -25,14 +25,12 @@ describe('User', () => {
 
   it('GET request for user sign-up', () => {
     return request(app)
-    .get('/api/v1/users/login?p=108443533200110515478')
+    .get('/api/v1/users/signup?p=108443533200110515478')
     .then(response => {
       expect(response.statusCode).toBe(200)
+      console.log(response.body)
       expect(Object.keys(response.body)).toContain('id')
       expect(Object.keys(response.body)).toContain('key')
-      expect(Object.keys(response.body.locations[0])).toContain('name')
-      expect(Object.keys(response.body.locations[0])).toContain('url')
-      expect(Object.keys(response.body.locations[0])).toContain('hours')
     })
   });
 

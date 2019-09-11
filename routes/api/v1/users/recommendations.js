9e49f84f065eead07960c332572a18c83bb3a54c
Martin Mercer <28820023+m-mrcr@users.noreply.github.com>
2019-09-11 15:46:46 -0600

    comments out functionality in progress

--- routes/api/v1/users/recommendations.js
+++ routes/api/v1/users/recommendations.js
@@ -27,50 +27,50 @@ router.get('/:id', async function(req, res) {
   }
 });
 
-router.post('/:id', async function(req, res) {
-  try {
-    let id = req.params.id;
-    let yelpId = req.query.yelpId;
-    let headers = {"Authorization": `Bearer ${process.env.YELP_API_KEY}`}
-    let results = await fetch(`https://api.yelp.com/v3/businesses/${yelpId}`, { method: 'GET', headers: headers })
-      .then(response => response.json())
-      .catch(error => error)
-    // var serialized = serializer.LocationSerializer(results)
-    // let user = await User.findOne({where: {id: id}})
-    // let location = await Location.create({
-    //   name: serialized.name,
-    //   image: serialized.image_url,
-    //   url: serialized.url,
-    //   phone: serialized.display_phone,
-    //   rating: serialized.rating,
-    //   reviewCount: serialized.review_count,
-    //   categories: serialized.categories,
-    //   coordinates: serialized.coordinates,
-    //   location: serialized.location,
-    //   hours: serialized.hours,
-    //   price: serialized.price,
-    //   yelpId: serialized.yelpId
-    // })
-    // UserLocation.create({
-    //   userId: user.id,
-    //   locationId: location.id,
-    //   notes: ''
-    // })
-    res.setHeader(...defaultHeader)
-    res.status(201).send(JSON.stringify(results))
-    } catch (error) {
-      res.setHeader(...defaultHeader)
-      res.status(500).send({ error })
-    }
-  }
-);
-
-router.delete('/:id', async function(req, res) {
-  try {
-    User.findOne({where: {key: req.params.id}})
-  } catch {
-
-  }
-})
+// router.post('/:id', async function(req, res) {
+//   try {
+//     let id = req.params.id;
+//     let yelpId = req.query.yelpId;
+//     let headers = {"Authorization": `Bearer ${process.env.YELP_API_KEY}`}
+//     let results = await fetch(`https://api.yelp.com/v3/businesses/${yelpId}`, { method: 'GET', headers: headers })
+//       .then(response => response.json())
+//       .catch(error => error)
+//     // var serialized = serializer.LocationSerializer(results)
+//     // let user = await User.findOne({where: {id: id}})
+//     // let location = await Location.create({
+//     //   name: serialized.name,
+//     //   image: serialized.image_url,
+//     //   url: serialized.url,
+//     //   phone: serialized.display_phone,
+//     //   rating: serialized.rating,
+//     //   reviewCount: serialized.review_count,
+//     //   categories: serialized.categories,
+//     //   coordinates: serialized.coordinates,
+//     //   location: serialized.location,
+//     //   hours: serialized.hours,
+//     //   price: serialized.price,
+//     //   yelpId: serialized.yelpId
+//     // })
+//     // UserLocation.create({
+//     //   userId: user.id,
+//     //   locationId: location.id,
+//     //   notes: ''
+//     // })
+//     res.setHeader(...defaultHeader)
+//     res.status(201).send(JSON.stringify(results))
+//     } catch (error) {
+//       res.setHeader(...defaultHeader)
+//       res.status(500).send({ error })
+//     }
+//   }
+// );
+//
+// router.delete('/:id', async function(req, res) {
+//   try {
+//     User.findOne({where: {key: req.params.id}})
+//   } catch {
+//
+//   }
+// })
 
 module.exports = router;
